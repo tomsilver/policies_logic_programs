232ba1ca66d38cf211f7e3b33ce47798841c2bcc
Tom Silver <tom@dhcp-18-21-185-171.dyn.mit.edu>
2020-02-25 15:59:19 -0500

    missing renaming of checkmate tactic (thanks TommasoBendinelli)

--- env_settings.py
+++ env_settings.py
@@ -1,5 +1,5 @@
 from generalization_grid_games.envs import two_pile_nim as tpn
-from generalization_grid_games.envs import checkmate_tactic
+from generalization_grid_games.envs import checkmate_tactic as ct
 from generalization_grid_games.envs import stop_the_fall as stf
 from generalization_grid_games.envs import chase as ec
 from generalization_grid_games.envs import reach_for_the_star as rfts
