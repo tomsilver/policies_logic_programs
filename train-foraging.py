5a5a71ccb15a3d535dbaf48914ed02405a3829d9
Manuel Eberhardinger <eberhardinger@hdm-stuttgart.de>
2022-04-29 09:56:56 +0200

    print args

--- train-foraging.py
+++ train-foraging.py
@@ -6,19 +6,19 @@ import argparse
 import os
 
 parser = argparse.ArgumentParser(description='Train the logical program policies on the ForagingEnv')
-parser.add_argument('n_demos', type=int, default=10,
+parser.add_argument('--n_demos', type=int, default=10,
                     help='an integer for the accumulator')
-parser.add_argument('num_programs', type=int, default=10000,
+parser.add_argument('--num_programs', type=int, default=10000,
                     help='the number of feature detector programs')
-parser.add_argument('num_dt', type=int, default=5,
-                    help='the number of feature detector programs')
-parser.add_argument('max_num_particles', type=int, default=50,
-                    help='the number of feature detector programs')
-parser.add_argument('gen_prog_step_size', type=int, default=1,
-                    help='the number of feature detector programs')
-
+parser.add_argument('--num_dt', type=int, default=5,
+                    help='the number of decision trees')
+parser.add_argument('--max_num_particles', type=int, default=50,
+                    help='the max number of particles for the decision tree')
+parser.add_argument('--gen_prog_step_size', type=int, default=1,
+                    help='the step size of the program generation loop')
 
 args = parser.parse_args()
+print('Train policy with args:', ' '.join(f'{k}={v}' for k, v in vars(args).items()))
 
 policy_file_name = f'policy-{args.n_demos}-{args.num_programs}-{args.num_dt}-{args.max_num_particles}-{args.gen_prog_step_size}.pkl'
 
