ef89e2239abf6eaecfbe351b20f2a0b06c3e8d10
Manuel Eberhardinger <eberhardinger@hdm-stuttgart.de>
2022-05-19 15:44:22 +0200

    install script

---
+++ install-requirements.sh
@@ -0,0 +1,10 @@
+pip install -e git+https://github.com/tomsilver/generalization_grid_games.git@8a03c057c7a3d0921337fb89c6d3e3c4fe15a21d#egg=generalization_grid_games
+pip install gym==0.18.0
+pip install imageio
+pip install -e git+https://github.com/ManuelEberhardinger/lb-foraging.git@a2e7de5566123c45b0058bf0d42af0cbd6a21dbc#egg=lbforaging
+pip install matplotlib
+pip install numpy
+pip install pandas
+pip install scikit-learn
+pip install scipy
+pip install tqdm
